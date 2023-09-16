e55df5f7b7bc0dd061dcb28716b12e493f20fa6f
Hazem Hamaad <hazem.hamaad@outlook.com>
2023-09-16 15:59:28 +0300

    update login page template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,85 +18,148 @@
 
     <link href="{{ asset('plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css"/>
     <link href="{{ asset('css/style.bundle.css') }}" rel="stylesheet" type="text/css"/>
-    <link href="{{ asset('css/custom.style.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet" type="text/css"/>
 
 </head>
 
 <body id="kt_body" class="app-blank">
 <div class="d-flex flex-column flex-root" id="kt_app_root">
 
-    <div class="d-flex flex-column flex-lg-row flex-column-fluid">
-        <a class="d-block d-lg-none mx-auto py-20">
-            <img alt="Logo" src="{{ asset('media/logos/logo-icon.png') }}" class="theme-light-show h-100px"/>
-        </a>
+    <div class="d-flex flex-column flex-root" id="kt_app_root">
 
-        <div class="d-flex flex-column flex-column-fluid flex-center w-lg-50 p-10">
-            <div class="d-flex justify-content-between flex-column-fluid flex-column w-100 mw-450px">
-                <div class="d-flex flex-stack py-2" style="justify-content: center">
-                    <img src="{{ asset('media/logos/logo-en-text.png') }}" alt="logo" width="200px">
-                </div>
-                <div class="py-20 login-form">
+        <!--begin::Authentication - Sign-in -->
+        <div class="d-flex flex-column flex-lg-row flex-column-fluid">
+            <!--begin::Body-->
+            <div class="d-flex flex-column flex-lg-row-fluid w-lg-50 p-10 order-2 order-lg-1">
+                <!--begin::Form-->
+                <div class="d-flex flex-center flex-column flex-lg-row-fluid">
+                    <!--begin::Wrapper-->
+                    <div class="w-lg-500px p-10">
 
-                    <form class="form w-100" novalidate="novalidate" method="POST" id="kt_sign_in_form"
-                          action="{{ route('admin.login.submit') }}">
-                        {{ csrf_field() }}
-                        <!--begin::Heading-->
-                        <div class="text-center mb-11">
-                            <!--begin::Title-->
-                            <h1 class="text-dark fw-bolder mb-3">Sign In</h1>
-                            <!--end::Title-->
-                            <!--begin::Subtitle-->
-                        </div>
-                        <!--begin::Heading-->
-                        <!--begin::Input group=-->
-                        <div class="fv-row mb-8">
-                            <!--begin::Email-->
-                            <input type="text" placeholder="Email" name="email" autocomplete="off"
-                                   class="form-control bg-transparent"/>
-                            <!--end::Email-->
-                        </div>
-                        <!--end::Input group=-->
-                        <div class="fv-row mb-3">
-                            <!--begin::Password-->
-                            <input type="password" placeholder="Password" name="password" autocomplete="off"
-                                   class="form-control bg-transparent"/>
-                            <!--end::Password-->
-                        </div>
-                        <!--end::Input group=-->
-                        <!--begin::Wrapper-->
-                        <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
-                            <div></div>
-                            <!--begin::Link-->
-                            <!--end::Link-->
-                        </div>
-                        <!--end::Wrapper-->
-                        <!--begin::Submit button-->
-                        <div class="d-grid mb-10">
-                            <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
-                                <!--begin::Indicator label-->
-                                <span class="indicator-label">Sign In</span>
-                                <!--end::Indicator label-->
-                                <!--begin::Indicator progress-->
-                                <span class="indicator-progress"> Please wait...
+                        <!--begin::Form-->
+                        <form class="form w-100" novalidate="novalidate" method="POST" id="kt_sign_in_form"
+                              action="{{ route('admin.login.submit') }}">
+                            {{ csrf_field() }}
+                            <!--begin::Heading-->
+                            <div class="text-center mb-11">
+                                <!--begin::Title-->
+                                <h1 class="text-dark fw-bolder mb-3">
+                                    Sign In
+                                </h1>
+                                <!--end::Title-->
+                            </div>
+                            <!--begin::Heading-->
+
+                            <!--begin::Input group--->
+                            <div class="fv-row mb-8 fv-plugins-icon-container">
+                                <!--begin::Email-->
+                                <input type="text" placeholder="Email" name="email" autocomplete="off"
+                                       class="form-control bg-transparent"/>
+                                <!--end::Email-->
+                                <div
+                                    class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div>
+                            </div>
+
+                            <!--end::Input group--->
+                            <div class="fv-row mb-3 fv-plugins-icon-container">
+                                <!--begin::Password-->
+                                <input type="password" placeholder="Password" name="password" autocomplete="off"
+                                       class="form-control bg-transparent"/>
+                                <!--end::Password-->
+                                <div
+                                    class="fv-plugins-message-container fv-plugins-message-container--enabled invalid-feedback"></div>
+                            </div>
+                            <!--end::Input group--->
+
+                            <!--begin::Wrapper-->
+                            <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
+                                <div></div>
+
+                                <!--begin::Link-->
+                                <a href="/metronic8/demo1/../demo1/authentication/layouts/corporate/reset-password.html"
+                                   class="link-primary">
+                                    Forgot Password ?
+                                </a>
+                                <!--end::Link-->
+                            </div>
+                            <!--end::Wrapper-->
+
+                            <!--begin::Submit button-->
+                            <div class="d-grid mb-10">
+                                <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
+                                    <!--begin::Indicator label-->
+                                    <span class="indicator-label">Sign In</span>
+                                    <!--end::Indicator label-->
+                                    <!--begin::Indicator progress-->
+                                    <span class="indicator-progress"> Please wait...
 										<span class="spinner-border spinner-border-sm align-middle ms-2"></span></span>
-                                <!--end::Indicator progress-->
-                            </button>
-                        </div>
-                        <!--end::Submit button-->
-                    </form>
-
+                                    <!--end::Indicator progress-->
+                                </button>
+                            </div>
+                            <!--end::Submit button-->
+                        </form>
+                        <!--end::Form-->
+                    </div>
+                    <!--end::Wrapper-->
                 </div>
+                <!--end::Form-->
 
-                <div class="m-0">
+                <!--begin::Footer-->
+                <div class="w-lg-500px d-flex flex-stack px-10 mx-auto">
+                    <!--begin::Links-->
+                    <div class="d-flex fw-semibold text-primary fs-base gap-5">
+                        <a href="" target="_blank">Terms</a>
+
+                        <a href="" target="_blank">Plans</a>
+
+                        <a href="" target="_blank">Contact Us</a>
+                    </div>
+                    <!--end::Links-->
                 </div>
+                <!--end::Footer-->
             </div>
-        </div>
+            <!--end::Body-->
 
-        <div
-            class="d-none d-lg-flex flex-lg-row-fluid w-50 bgi-size-cover bgi-position-y-center bgi-position-x-start bgi-no-repeat"
-            style="background-image: url({{ asset('media/auth/bg11.png') }})">
+            <!--begin::Aside-->
+            <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2"
+                 style="background-image: url({{ asset('media/backgrounds/YdvCNW.webp') }})">
+                <!--begin::Content-->
+                <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
+                    <!--begin::Logo-->
+                    <a class="mb-50 mb-lg-50">
+                    </a>
+                    <!--end::Logo-->
+
+                    <!--begin::Image-->
+                    <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20"
+                         src="{{ asset('media/misc/auth-screens.png') }}" alt="">
+                    <!--end::Image-->
+
+                    <!--begin::Title-->
+                    <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">
+                        Fast, Efficient and Productive
+                    </h1>
+                    <!--end::Title-->
+
+                    <!--begin::Text-->
+                    <div class="d-none d-lg-block text-white fs-base text-center">
+                        In this kind of post,
+
+                        introduces a person they’ve interviewed <br> and provides some background information about
+
+                        and their <br> work following this is a transcript of the interview.
+                    </div>
+                    <!--end::Text-->
+                </div>
+                <!--end::Content-->
+            </div>
+            <!--end::Aside-->
         </div>
+        <!--end::Authentication - Sign-in-->
+
+
     </div>
+
 </div>
 
 <!--begin::Javascript-->
